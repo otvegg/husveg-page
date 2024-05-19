1e4f3a96283545629b2a13268566aca794874158
otvegg <timian@husveg.net>
2024-05-20 04:10:54 +1000

    Added map and fixed iterating over images

--- src/components/pages/Bris.tsx
+++ src/components/pages/Bris.tsx
@@ -12,22 +12,75 @@ import {
   useDisclosure,
   ModalBody,
   ModalHeader,
+  Link,
+  AspectRatio,
+  ListItem,
+  UnorderedList,
 } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import React, { useState } from 'react';
 import someimg from '../../resources/bris/common/bris_0.jpg';
 import backgroundImage from '../../resources/bris/repeating_cubes.jpg';
+import {
+  bris_bad1,
+  soverom_1,
+  soverom_2,
+  stue_1,
+  stue_2,
+  stue_3
+} from '../../resources/bris/index'
+
+
+const imageList = [
+  { src: bris_bad1, description: 'Baderom (oppvaskum)' },
+  { src: soverom_1, description: 'Soverom 1, køyeseng' },
+  { src: soverom_2, description: 'Soverom 2, køyeseng' },
+  { src: stue_1, description: 'Kjøkken og spisebord' },
+  { src: stue_2, description: 'Sofa med sofabord og TV' },
+  { src: stue_3, description: 'Godt med naturlig lys' },
+];
+
+
 function Bris() {
-  // Get all image links from an api call?
-  const images = require.context('../../resources/bris/common');
   const [showImages, setShowImages] = useState<boolean>(false);
+  const [imageCount, setImageCount] = useState<number>(0);
   const modal = useDisclosure();
   console.log(showImages, setShowImages);
+
+
+  const incrementImages = () => {
+    setImageCount((imageCount + 1) % imageList.length);
+  };
+
+  const decrementImages = () => {
+    setImageCount((imageCount - 1 + imageList.length) % imageList.length);
+  };
+
+  function extraInfo() {
+    return (
+      <Box textAlign="left" w='80%'>
+        <Text>Ekstra Info:</Text>
+        <UnorderedList>
+          <ListItem>
+            <Link href='https://no.wikipedia.org/wiki/S%C3%B8ndre_Sand%C3%B8y' color='teal.500' isExternal>
+              Wikipedia om Søndre Sandøy <ExternalLinkIcon mx='2px' />
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href='https://livetleker.wordpress.com/2009/06/20/saltkrakan-s%C3%B8ndre-sand%C3%B8y-eller-deromkring/' color='teal.500' isExternal>
+              Beskrivelse av tur til Søndre Sandøy <ExternalLinkIcon mx='2px' />
+            </Link>
+          </ListItem>
+        </UnorderedList>
+      </Box>
+    );
+  }
+
   return (
     <>
       <Flex h='100vh' flexDir='column' alignItems='center' bgImage={backgroundImage} backgroundRepeat={'repeat'}>
         <Flex h='full' w='80%' flexDir='column' alignItems='center' bg={useColorModeValue('white', '#2d2e30')}>
-          <Box w='full' h='30vh' bgImage={someimg} bgPosition={'-90px -500px'} /*  w='full' h='30vh !important' */>
-            {/* <Image src={someimg} objectFit={'contain'} w='200px' h='50px' /> */}
+          <Box w='full' h='30vh' bgImage={someimg} bgPosition={'-90px -500px'} >
           </Box>
           <Heading fontSize={'30px'} fontWeight={'bold'}>
             Bris
@@ -40,51 +93,46 @@ function Bris() {
               onClick={() => {
                 modal.isOpen ? modal.onClose() : modal.onOpen();
               }}>
-              Click me to show picture
+              Bla gjennom bilder
             </Button>
+            <Divider w='80%' h='1px' m='10px' borderColor={useColorModeValue('#2d2e30', 'white')} />
+            {extraInfo()}
+            <Divider w='80%' h='1px' m='10px' borderColor={useColorModeValue('#2d2e30', 'white')} />
+
+
+            <iframe width="425" height="350"
+              src="https://www.openstreetmap.org/export/embed.html?bbox=11.078735589981079%2C59.00107209435399%2C11.082785725593569%2C59.003561281295184&amp;layer=mapnik&amp;marker=59.00231671032385%2C11.080760657787323"
+            >
+            </iframe>
+            <Button colorScheme="teal" onClick={() => window.open('https://www.openstreetmap.org/?mlat=59.00231&amp;mlon=11.08077#map=18/59.00231/11.08077', '_blank')}>
+              View Large map
+            </Button>
+
+
+
             <Modal isOpen={modal.isOpen} onClose={modal.onClose}>
-              <ModalContent bg={useColorModeValue('white', '#2d2e30')} top='20vh'>
-                <ModalHeader /* align='center' */ h='40px' p='5px'>
-                  Modal header
+              <ModalContent bg={useColorModeValue('white', '#2d2e30')} top='5vh'>
+                <ModalHeader h='40px' p='5px'>
+                  Bris bilder
                 </ModalHeader>
-                <ModalBody>
-                  <Flex alignItems={'center'} justifyContent='center'>
-                    <Button bg='transparent' variant='iterateImagesButton'>
+                <ModalBody minH='250px' minW='200px'>
+                  <Flex alignItems={'center'} justifyContent='center' flexDir='row'>
+                    <Button bg='transparent' onClick={decrementImages} variant='iterateImagesButton'>
                       {'<'}
                     </Button>
-                    <Flex flexDir='column'>
-                      <Image w='30vh' src={images(`./bris_${0}.jpg`)} objectFit={'contain'} />
-                      <Text align='center'>Some description</Text>
+                    <Flex flexDir='column' w='100%'>
+                      <AspectRatio maxW='600px' ratio={12 / 19}>
+                        <Image w='30vh' src={imageList[imageCount].src} objectFit={'contain'} />
+                      </AspectRatio>
+                      <Text align='center'>{imageList[imageCount].description}</Text>
                     </Flex>
-                    <Flex>
-                      <Button bg='transparent' fontSize={'40px'} fontWeight='bold'>
-                        {'>'}
-                      </Button>
-                    </Flex>
+                    <Button bg='transparent' onClick={incrementImages} fontSize={'40px'} fontWeight='bold'>
+                      {'>'}
+                    </Button>
                   </Flex>
                 </ModalBody>
               </ModalContent>
             </Modal>
-
-            {
-              // Iterate over data object
-              /* Object.keys(data).map((keyname, keyindex)=> {
-
-                    //let code = data[keyname]["code"];
-                    //let title = data[keyname]["title"];
-                    // Dynamically reference image
-
-                    let imgsrc = images(`./bris_${keyindex}.png`);
-
-          
-                    return <Image  key={keyindex} src={imgsrc}  objectFit={'contain'} />
-                               
-                                
-                                
-                                
-                           
-                }) */
-            }
           </Flex>
         </Flex>
       </Flex>
