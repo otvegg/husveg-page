e65d05708d0a62bb37b532abfe682cbc27d5d6ed
otvegg <timian@husveg.net>
2024-05-20 04:09:51 +1000

    For media

---
+++ src/resources/bris/index.tsx
@@ -0,0 +1,15 @@
+import bris_bad1 from './slideshow/bris_bad1.jpg';
+import soverom_1 from './slideshow/soverom_1.jpg';
+import soverom_2 from './slideshow/soverom_2.jpg';
+import stue_1 from './slideshow/stue_1.jpg';
+import stue_2 from './slideshow/stue_2.jpg';
+import stue_3 from './slideshow/stue_3.jpg';
+
+export {
+    bris_bad1,
+    soverom_1,
+    soverom_2,
+    stue_1,
+    stue_2,
+    stue_3
+};
